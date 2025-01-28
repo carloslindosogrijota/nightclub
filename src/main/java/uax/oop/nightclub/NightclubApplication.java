03b2e5384c799f90b3e14ed85f4b86fe424a5dda
Mario Rodriguez <mario.rodriguez.2006@protonmail.com>
2025-01-28 12:36:05 +0100

    incio de proyecto

---
+++ src/main/java/uax/oop/nightclub/NightclubApplication.java
@@ -0,0 +1,13 @@
+package uax.oop.nightclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uax/oop/nightclub/NightclubApplicationTests.java
@@ -0,0 +1,13 @@
+package uax.oop.nightclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
