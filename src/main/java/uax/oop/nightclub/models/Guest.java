a0434f3806c849d4d5b3b0980f362ed0b7d79b48
Mario Rodriguez <mario.rodriguez.2006@protonmail.com>
2025-01-28 13:21:11 +0100

    cambios

---
+++ src/main/java/uax/oop/nightclub/models/Guest.java
@@ -0,0 +1,44 @@
+package uax.oop.nightclub.models;
+
+public class Guest {
+    private String dni;
+    private String name;
+    private int age;
+    private boolean hasReservation;
+    public Object se;
+    public Guest(String dni, String name, int age, boolean hasReservation) {
+        this.dni = dni;
+        this.name = name;
+        this.age = age;
+        this.hasReservation = hasReservation;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public boolean isHasReservation() {
+        return hasReservation;
+    }
+    public void setHasReservation(boolean hasReservation) {
+        this.hasReservation = hasReservation;
+    }
+    @Override
+    public String toString() {
+        return "Guest [dni=" + dni + ", name=" + name + ", age=" + age + ", hasReservation=" + hasReservation + "]";
+    }
+    
+}
