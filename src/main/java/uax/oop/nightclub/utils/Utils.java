da17384454fef3118ffb504c0221d85bcaddb742
Carlos Lindoso <clgredes@proton.me>
2025-02-04 20:37:29 +0100

    Ultimos cambios (Explicado en reunión)

--- src/main/java/uax/oop/nightclub/utils/Utils.java
+++ src/main/java/uax/oop/nightclub/utils/Utils.java
@@ -1,13 +1,37 @@
 package uax.oop.nightclub.utils;
 
+import java.util.ArrayList;
+
+import uax.oop.nightclub.models.Guest;
+import uax.oop.nightclub.models.Order;
+import uax.oop.nightclub.models.Table;
+
 public class Utils {
 
-    // Funcionalidad 2:
-    // Método para añadir mesas VIP a 'x' persona/s.
-    // Método para añadir mesas normales a 'x' persona/s.
-    // Método para mostrar mesas disponibles.
+    // Funcionalidad 1:
 
+// --------------------------------------------------------------------
+    // Funcionalidad 2:
+    // Método para añadir mesas VIP o normales a 'x' persona/s.
+    // Método para mostrar mesas disponibles.
+    public Guest tableManagment(Table myTable, Guest myGuest) {
+
+        return myGuest;
+    }
+    public ArrayList<Table> viewTables(ArrayList<Table> myTablesList) {
+
+        return myTablesList;
+    }
+// --------------------------------------------------------------------
+    // Funcionalidad 3:
+
+// --------------------------------------------------------------------
     // Funcionalidad 4:
     // Método para realizar pedidos asociados a mesas
     // - calcular costo total de dichos
+    public Order tablesOrders(Order myOrder, Table myTable) {
+
+        return myOrder;
+    }
+// --------------------------------------------------------------------
 }
