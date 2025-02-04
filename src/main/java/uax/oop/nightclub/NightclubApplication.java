5db8c14d19af1c154b6a4eb9e64169a63b940c93
Carlos Lindoso <clgredes@proton.me>
2025-02-04 20:19:15 +0100

    Borro items del main

--- src/main/java/uax/oop/nightclub/NightclubApplication.java
+++ src/main/java/uax/oop/nightclub/NightclubApplication.java
@@ -1,15 +1,11 @@
 package uax.oop.nightclub;
 
-import uax.oop.nightclub.models.Guest;
-import uax.oop.nightclub.utils.Tools;
+import uax.oop.nightclub.utils.Utils;
 public class NightclubApplication {
 
 	public static void main(String[] args) {
 
-		Tools t = new Tools();
-		Guest g1 = new Guest("23874348A", "Pepe", 18, false);
-		Guest g2 = new Guest("37347347B", "Pepa", 20, false);
-		Guest g3 = new Guest("61523653X", "Raúl", 19, true);
+		Utils myTools = new Utils();
 
 	}
 
