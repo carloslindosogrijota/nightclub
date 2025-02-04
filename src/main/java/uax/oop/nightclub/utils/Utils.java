6dfe77c9ecb2453ade673cb5e33fcbb65fee2857
Carlos Lindoso <clgredes@proton.me>
2025-02-04 20:00:05 +0100

    Funciones carlos

--- src/main/java/uax/oop/nightclub/utils/Utils.java
+++ src/main/java/uax/oop/nightclub/utils/Utils.java
@@ -2,6 +2,12 @@ package uax.oop.nightclub.utils;
 
 public class Utils {
 
+    // Funcionalidad 2:
+    // Método para añadir mesas VIP a 'x' persona/s.
+    // Método para añadir mesas normales a 'x' persona/s.
+    // Método para mostrar mesas disponibles.
 
-
+    // Funcionalidad 4:
+    // Método para realizar pedidos asociados a mesas
+    // - calcular costo total de dichos
 }
