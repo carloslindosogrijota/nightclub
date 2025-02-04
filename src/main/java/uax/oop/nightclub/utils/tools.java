fbe8b98e4b34861179561bdaaa696cadc6e1c596
Carlos Lindoso <clgredes@proton.me>
2025-02-04 19:04:31 +0100

    Recreación de clase tools

--- src/main/java/uax/oop/nightclub/utils/tools.java
+++ src/main/java/uax/oop/nightclub/utils/tools.java
@@ -1,21 +1,7 @@
 package uax.oop.nightclub.utils;
 
-import java.util.ArrayList;
+import uax.oop.nightclub.models.Guest;
 
-import uax.oop.nightclub.models.*;
+public class Tools {
 
-public class tools {
-    public boolean checkAge(Guest client){
-        return client.getAge() >= 18 ? true : false;
-    }
-    public Guest reserveTable(Guest c, ArrayList<Guest> arr){
-        for (Guest x : arr) {
-            if (x.getDni().equals(c.getDni()) && x.isHasReservation() == false) {
-                x.setHasReservation(true);
-            } else if (x.getDni().equals(c.getDni()) && x.isHasReservation() == true){
-                throw new Error("El cliente ya tiene una reserva ");
-            }
-        }
-                return c;
-    }
 }
