964eb51c89cfd77eedd95895a2b2b47f1a1d8442
Mario Rodriguez <mario.rodriguez.2006@protonmail.com>
2025-02-05 13:27:42 +0100

    Update Utils.java

--- src/main/java/uax/oop/nightclub/utils/Utils.java
+++ src/main/java/uax/oop/nightclub/utils/Utils.java
@@ -7,9 +7,13 @@ import uax.oop.nightclub.models.Order;
 import uax.oop.nightclub.models.Table;
 
 public class Utils {
-
+    
+// --------------------------------------------------------------------
     // Funcionalidad 1:
 
+// --------------------------------------------------------------------
+
+    
 // --------------------------------------------------------------------
     // Funcionalidad 2:
     // Método para añadir mesas VIP o normales a 'x' persona/s. Y comprobar que no se coja una mesa ya reservada.
@@ -26,13 +30,20 @@ public class Utils {
     // Funcionalidad 3:
 
 // --------------------------------------------------------------------
+
+    
+// --------------------------------------------------------------------
     // Funcionalidad 4:
     // Método para realizar pedidos asociados a mesas
     // Método para calcular costo total de los pedidos de una mesa.
     public Table tablesOrders(Order myOrder, Table myTable) {
 
-        return myTable.get;
+        return myTable.get(ordersOfTable);
     }
-    public double 
+    public double orderTotalPriceOfTable (Table tablesOrders) {
+        double result;
+        
+        return result;
+    }
 // --------------------------------------------------------------------
 }
