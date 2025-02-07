70c26cb925ec68e74b38e876d797632bb3a412ce
Carlos Lindoso <clgwork@proton.me>
2025-02-07 10:11:46 +0100

    Utils - func2 - modificada

--- src/main/java/uax/oop/nightclub/utils/Utils.java
+++ src/main/java/uax/oop/nightclub/utils/Utils.java
@@ -8,63 +8,65 @@ import uax.oop.nightclub.models.Table;
 
 public class Utils {
 
-// --------------------------------------------------------------------
-// Funcionalidad 1:
+    // --------------------------------------------------------------------
+    // Funcionalidad 1:
 
-// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
-
-// --------------------------------------------------------------------
-// Funcionalidad 2:
-    // Método para añadir mesas VIP o normales a 'x' persona/s, comprobar que no se coja una mesa ya reservada, y comprobar que la capacidad de personas en la mesa no se exceda.
+    // --------------------------------------------------------------------
+    // Funcionalidad 2:
+    // Método para añadir mesas VIP o normales a 'x' persona/s, comprobar que no se
+    // coja una mesa ya reservada, y comprobar que la capacidad de personas en la
+    // mesa no se exceda.
     public String tableManagment(Table myTable, ArrayList<Guest> myGuestList) throws Exception {
-        String resultado="";
-        if(myTable.isIsAvailable()==true && myGuestList.size()<myTable.getCapacity() && myTable.isIsVip()){
+        String resultado = "";
+        if (myTable.isIsAvailable() == true && myGuestList.size() < myTable.getCapacity() && myTable.isIsVip()) {
             myTable.getGuestTablesReserved().addAll(myGuestList);
             myTable.setIsAvailable(false);
             resultado += "The guests of the list have added to the VIP table " + myTable.getTableId();
-        }
-        if(myTable.isIsAvailable()==true && myGuestList.size()<myTable.getCapacity() && !myTable.isIsVip()){
+        } else if (myTable.isIsAvailable() == true && myGuestList.size() < myTable.getCapacity()
+                && !myTable.isIsVip()) {
             myTable.getGuestTablesReserved().addAll(myGuestList);
             myTable.setIsAvailable(false);
             resultado += "The guests of the list have added to the table " + myTable.getTableId();
-        } else if (myTable.isIsAvailable()==true && myGuestList.size()>myTable.getCapacity()){
-            throw new Exception ("The guests of the list exceeded the permitted number of guests");
-        }else {
-            throw new Exception ("The table isn't avaible");
+        } else if (myTable.isIsAvailable() == true && myGuestList.size() > myTable.getCapacity()) {
+            throw new Exception("The guests of the list exceeded the permitted number of guests");
+        } else {
+            throw new Exception("The table isn't avaible");
         }
         return resultado;
     }
-    //Método para mostrar la Lista de tablas disponibles.
+
+    // Método para mostrar la Lista de tablas disponibles.
     public ArrayList<String> viewTables(ArrayList<Table> myTablesList) {
         ArrayList<String> myAvailableTables = new ArrayList<>();
         for (Table x : myTablesList) {
-            if(x.isIsAvailable()){
+            if (x.isIsAvailable()) {
                 myAvailableTables.add(x.toString());
             }
         }
         return myAvailableTables;
     }
-// --------------------------------------------------------------------
-// Funcionalidad 3:
+    // --------------------------------------------------------------------
+    // Funcionalidad 3:
 
-// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
-
-// --------------------------------------------------------------------
-// Funcionalidad 4:
+    // --------------------------------------------------------------------
+    // Funcionalidad 4:
     // Método para realizar pedidos asociados a mesas
     public String tablesOrders(ArrayList<Order> myOrdersList, Table myTable) {
         myTable.getOrdersOfTable().addAll(myOrdersList);
         return "The following orders have been added to the " + myTable;
     }
+
     // Método para calcular costo total de los pedidos de una mesa.
-    public double orderTotalPriceOfTable (Table myTable) {
+    public double orderTotalPriceOfTable(Table myTable) {
         double result = 0;
         for (Order x : myTable.getOrdersOfTable()) {
             result += x.getTotalPrice();
         }
         return result;
     }
-// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 }
